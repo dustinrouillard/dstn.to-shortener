f9159a3ee3fcf05b51294f884a3a7ec26dab4d9b
Dustin Rouillard <me@dustin.sh>
2020-04-14 07:58:42 -0700

    Add 404

--- lib/app.ts
+++ lib/app.ts
@@ -7,6 +7,10 @@ const app = express();
 
 app.use('/g', Go);
 
+app.use((_, res: express.Response) => {
+    return res.status(404).send({ code: 'page_not_found', message: 'Route not found' });
+});
+
 app.listen(App.Port, () => {
     console.log(`Server ready on ${App.Port}`);
 });
--- lib/routes/go.ts
+++ lib/routes/go.ts
@@ -1,22 +1,29 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 
 const route = Router();
 
 // Temporary object of shortend links
 const links: any = {
   twitter: "https://twitter.com/dustinrouillard",
+  twitch: "https://twitch.tv/dustinrouillard",
   discord: "https://dustin.vc",
+  hiven: "https://hiven.house/dustin",
+  notify: "https://notify.me/dustin",
   snapchat: "https://snapchat.com/add/dustinrouillard",
   linkedin: "https://linkedin.com/in/dustinrouillard",
+  github: "https://github.com/dustinrouillard",
+  keybase: "https://keybase.io/dustinrouillard"
 };
 
-route.get('/:code', (req: Request, res: Response) => {
+route.get('/:code', (req: Request, res: Response, next: NextFunction) => {
     let code: string = req.params.code;
 
     if (links[code]) {
         console.log(`Go - visit:${code} - link:${links[code]} - ip:${req.headers['CF-Connecting-IP'] || req.connection.remoteAddress}`);
         return res.redirect(302, links[code]);
     }
+
+    return next();
 });
 
 export default route;

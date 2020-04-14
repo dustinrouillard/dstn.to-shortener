af686621cb1013a26d02c0ae025746e692e379ba
Dustin Rouillard <me@dustin.sh>
2020-04-14 08:13:04 -0700

    Add base

--- lib/app.ts
+++ lib/app.ts
@@ -7,6 +7,11 @@ const app = express();
 
 app.use('/g', Go);
 
+app.get('/', (req: express.Request, res: express.Response) => {
+    console.log(`Base - visit:base  - ip:${req.headers['CF-Connecting-IP'] || req.connection.remoteAddress}`);
+    return res.redirect(302, "https://dustin.sh");
+});
+
 app.use((_, res: express.Response) => {
     return res.status(404).send({ code: 'page_not_found', message: 'Route not found' });
 });

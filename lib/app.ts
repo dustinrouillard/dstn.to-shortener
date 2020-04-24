529d27ae36f5fb1710dbd56bc03848bc74b1f6ee
Dustin Rouillard <me@dustin.sh>
2020-04-24 07:54:28 -0700

    Change to remove /g

--- lib/app.ts
+++ lib/app.ts
@@ -5,15 +5,15 @@ import { App } from './modules/config';
 
 const app = express();
 
-app.use('/g', Go);
+app.use('/', Go);
 
 app.get('/', (req: express.Request, res: express.Response) => {
     console.log(`Base - visit:base  - ip:${req.headers['CF-Connecting-IP'] || req.connection.remoteAddress}`);
     return res.redirect(302, "https://dustin.sh");
 });
 
-app.use((_, res: express.Response) => {
-    return res.status(404).send({ code: 'page_not_found', message: 'Route not found' });
+app.use((req: express.Request, res: express.Response) => {
+    return res.redirect(302, `https://dustin.sh${req.path}`);
 });
 
 app.listen(App.Port, () => {

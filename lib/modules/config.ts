a5f70a706eae1a5e6424db26e2de5086186a7bec
Dustin Rouillard <me@dustin.sh>
2020-04-29 03:05:17 -0700

    Fix for database variable

--- lib/modules/config.ts
+++ lib/modules/config.ts
@@ -7,9 +7,9 @@ export const InternalSecret = process.env.INTERNAL_SECRET || 'internal_secret_to
 export const PostgresConfig = {
     Host: process.env.POSTGRES_HOST || '127.0.0.1',
     Port: Number(process.env.POSTGRES_PORT) || 5432,
-    User: process.env.POSTGRES_USER,
-    Pass: process.env.POSTGRES_PASS,
-    Database: process.env.POSTGRES_DATABASE
+    User: process.env.POSTGRES_USER || 'postgres',
+    Pass: process.env.POSTGRES_PASS || 'docker',
+    Database: process.env.POSTGRES_DATABASE || 'postgres'
 };
 
 export const RedisConfig = {
